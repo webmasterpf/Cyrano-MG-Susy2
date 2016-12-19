f78a6512b2ed3ca0e495082a53315fd8d3b10833
webmasterpf-DG <webmaster@provence-formation.com>
2016-12-19 15:45:26 +0100

    MG: homepage, réglages et styles (2)

--- page-front.tpl.php
+++ page-front.tpl.php
@@ -1,27 +1,27 @@
-<!--     ______________________ LAYOUT HOMEPAGE VERTICAL D_______________________ -->
+<!-- ______________________ LAYOUT HOMEPAGE VERTICAL D_______________________ -->
 <?php
 global $theme_path;
 include ($theme_path.'/includes/inc_header.php');
-?>
-
-<!--  ______________________ CONTENU _______________________ -->
-
-          <?php if ($content_top): ?>
-            <div class="content-top">
-              <?php print $content_top; ?>
-            </div>   <!--  /content-top -->
-          <?php endif; ?>
-
+?>  
+ <!-- ______________________ CONTENU HAUT _______________________ -->
  
-            <div class="conteneur-flex">
-<!--        ______________________ PARTIE GAUCHE _______________________ -->
-
+   <?php if ($content_top): ?>
+     <div class="content-top">
+         <?php print $content_top; ?>
+     </div> <!--/#content-top -->
+ <?php endif; ?>
+ 
+<!--     <div class="conteneur-flex">-->
+   
+        	 
+<!-- ______________________ PARTIE GAUCHE _______________________ -->
+      
 	<div class="HPGauche">
-
+	 
         <?php if ($PartieGauche1): ?>
      <div class="HPGauche1"><?php print $PartieGauche1; ?></div>
         <?php endif; ?>
-
+		
      <?php if ($PartieGauche2): ?>
      <div class="HPGauche2"><?php print $PartieGauche2; ?></div>
         <?php endif; ?>
@@ -29,18 +29,17 @@ include ($theme_path.'/includes/inc_header.php');
      <?php if ($PartieGauche3): ?>
      <div class="HPGauche3"><?php print $PartieGauche3; ?></div>
         <?php endif; ?>
-
-   </div> 
-
-<!--______________________ ZONE DIAPO DROITE_______________________ -->
-
-               <?php if ($DiapoDroiteHP): ?>
+	  
+</div><!--    /Partie Gauche -->
+<!-- ______________________ ZONE DIAPO _______________________ -->
+			
+            <?php if ($DiapoDroiteHP): ?>
        <div class="HPDiapoVerticalDroite"><?php print $DiapoDroiteHP; ?></div>
               <?php endif; ?>
-
-</div>	<!-- /conteneur-flex -->
-<!--    ______________________ FIN LAYOUT PAGE _______________________ -->
-
+       
+<!--	</div> -->
+        <!--/conteneur-flex -->
+    <!-- ______________________ FIN LAYOUT PAGE _______________________ -->
 
 <!-- ______________________ CONTENU BAS _______________________ -->
     <?php if ($content_bottom_home): ?>
@@ -51,7 +50,11 @@ include ($theme_path.'/includes/inc_header.php');
 
 
     </div><!-- /#content-bottom -->
-        <?php
+   
+	
+
+</div> <!-- /content-global -->
+    <?php
 global $theme_path;
 include ($theme_path.'/includes/inc_footer.php');
-?>
+?>      
