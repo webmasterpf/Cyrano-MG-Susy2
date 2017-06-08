337bca9ad70afd7af4aad2218d7ae472958297db
webmasterpf-DG <webmaster@provence-formation.com>
2017-06-08 16:54:48 +0200

    MG: remplacement viewsslideshow par bxslider pour illus contenu lycée

--- node/node-page_lycee.tpl.php
+++ node/node-page_lycee.tpl.php
@@ -7,11 +7,24 @@
     <div class="node-inner">
         <!--______________COLONNE 1________________ -->
   
-        <div id="colonne-1" class="col1_layout_4_8 page-lycee">
+        <div id="colonne-1" class="col1_layout_3_6_3 page-lycee">
             <?php if ($title): /*copier le titre dans la colonne desirée*/?>
-            <h1 class="titre_page"><?php print $title; ?></h1>
+            <h1 class="titre-lycee"><?php print $title; ?></h1>
             <?php endif; ?>
-        
+       
+             <?php
+              global $theme_path;
+//              include ($theme_path.'/includes/dedicates_inc/inc_lycee_diapo.php');
+              ?>
+            
+          <?php  //Affichage du diapo d'illustration via un champ viewreference
+          if ($node->field_diapo_illus[0]['view']): ?>
+            <div class="diapo-lycee">
+                          <?php  print $node->field_diapo_illus[0]['view']  ?>
+            </div>
+            <?php endif;?>        
+            
+            
             <?php 
     global $theme_path;
 include($theme_path .'/includes/regions_inc/inc_region_col_1.php');
@@ -21,7 +34,7 @@ include($theme_path .'/includes/regions_inc/inc_region_col_1.php');
         </div>
         <!--______________COLONNE 2________________ -->
          <!-- <pre> <?php //print_r($node); ?> </pre>-->   <!-- listage des variables du $content -->
-        <div id="colonne-2"class="col2_layout_4_8 page-lycee">
+        <div id="colonne-2"class="col2_layout_3_6_3 page-lycee">
 
             <?php print $picture; ?>
 
@@ -37,6 +50,26 @@ include($theme_path .'/includes/regions_inc/inc_region_col_1.php');
   global $theme_path;
 include($theme_path .'/includes/regions_inc/inc_region_col_2.php');
 ?>
+             
+            </div>
+        </div>
+   
+                <!--______________COLONNE 3________________ -->
+        <div id="colonne-3" class="col3_layout_3_6_3 page-lycee">
+                     
+            
+             <?php
+              global $theme_path;
+              include ($theme_path.'/includes/dedicates_inc/inc_lycee_docs.php');
+              ?>
+            
+             <?php
+              global $theme_path;
+              include ($theme_path.'/includes/dedicates_inc/inc_gasquet_actus.php');
+              ?>
+            
+          
+   
                         <?php 
   //$theme_path = drupal_get_path('theme', 'NOM_THEME');
   global $theme_path;
@@ -44,8 +77,6 @@ include($theme_path .'/includes/regions_inc/inc_region_col_3.php');
 ?>
                 
             </div>
- 
-
       
         <?php if ($terms): ?>
         <div class="taxonomy"><?php //print $terms; ?></div>
