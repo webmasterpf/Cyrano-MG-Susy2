72e10e8d33cf42ec4b0de6fa4eea046dd1cc26f0
webmasterpf-DG <webmaster@provence-formation.com>
2017-06-21 10:04:49 +0200

    MG: Réglages pages maintenance

--- node/node-page_erreur.tpl.php
+++ node/node-page_erreur.tpl.php
@@ -6,17 +6,9 @@
 <div class="node <?php print $classes; ?>" id="node-<?php print $node->nid; ?>">
     <div class="node-inner">
         <!--______________COLONNE 1________________ -->
-        <?php /* choix du layout selon nombre de colonne
-         * .col1_layout_200_590_200{} .col1_layout_330_all{} .col1_layout_18_56_25{}
-         * .col2_layout_200_590_200{} .col2_layout_330_all{} .col2_layout_18_56_25{}
-         * .col3_layout_200_590_200{} .col3_layout_330_all{} .col3_layout_18_56_25{}
-         * .col1_layout_215_535_235{} .col2_layout_215_535_235{} .col3_layout_215_535_235{}
-         * .col1_layout_490_all{}  .col2_layout_490_all{}
-         * .col1_layout_295_385_295{} .col2_layout_295_385_295{} .col3_layout_295_385_295{}
-         */?>
-        <div id="colonne-1" class="col1_layout_200_590_200">
+         <div id="colonne-1" class="col1_layout_3_6_3 page-erreur">
             <?php if ($title): /*copier le titre dans la colonne desirée*/?>
-            <h1 class="titre_page"><?php print $title; ?></h1>
+            <h1 class="titre_page-erreur"><?php print $title; ?></h1>
             <?php endif; ?>
             
             <?php if ($node->field_erreur_illus[0]['view']): ?>
@@ -26,15 +18,14 @@
             <?php endif;?>
 
                 	
-            
-             <?php
-              global $theme_path;
-              include ($theme_path.'/includes/inc_region_col_G1.php');
-              ?>
+                               <?php 
+    global $theme_path;
+include($theme_path .'/includes/regions_inc/inc_region_col_1.php');
+?>
         </div>
         <!--______________COLONNE 2________________ -->
          <!-- <pre> <?php //print_r($node); ?> </pre>-->   <!-- listage des variables du $content -->
-        <div id="colonne-2" class="col2_layout_200_590_200">
+        <div id="colonne-2" class="col2_layout_3_6_3 page-erreur">
 
             <?php print $picture; ?>
 
@@ -46,28 +37,23 @@
                 <?php   print $node->content['body']['#value'];/*déplacer le contenu dans la colonne désirée*/ ?>
             </div>
 
+            
+                               <?php 
+    global $theme_path;
+include($theme_path .'/includes/regions_inc/inc_region_col_2.php');
+?>
         </div>
 
         <!--______________COLONNE 3________________ -->
-        <div id="colonne-3" class="col3_layout_200_590_200">
+        <div id="colonne-3" class="col3_layout_3_6_3 page-erreur">
            
              	
             <?php if ($node->field_erreur_actus[0]['view']): ?>
-            <div id="nom-css">
+            <div id="gasquet-actus">
                     <?php  print $node->field_erreur_actus[0]['view']  ?>
             </div>
             <?php endif;?>
-                
-                
-                
-                
-            <!--***********!!!!!!  EXEMPLE DE CHAMP CCK INCLUS AVEC CONDITION !!!!!!!!************ -->
-            <?php if ($node->nom_du_champ[0]['view']): ?>
-            <div id="nom-css">
-                    <?php  print $node->nom_du_champ[0]['view']  ?>
-            </div>
-            <?php endif;?>
-
+               
 
         </div>
 
